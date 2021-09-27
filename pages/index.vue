bf59977608dd862d8efcf2352f291d4a29922853
unknown <sfranckdabryn@gmail.com>
2021-09-27 22:51:57 +0100

    My First Projet with NUXTJs

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    <div class="row mx-0 c">
+      <div class="col-4 pos">
+        column
+      </div>
+      <div class="col-8">
+        <BlocProduct/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.pos{
+  position: sticky;
+  width: 260px;
+  min-width: 260px;
+}
+.c{
+  position: static;
+}
+</style>
